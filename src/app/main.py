813adfb2930dfa4ee0c07dc5d86bed6c39b7cc2a
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-10 02:16:56 +0300

    feat(main): add route for /health and /home

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from src.app.api.user.router import router as video_router
+from src.app.api.web import router as home_router
 
 app = FastAPI()
 
 app.include_router(video_router, prefix="/video", tags=["video"])
+app.include_router(home_router, prefix="", tags=["home"])
