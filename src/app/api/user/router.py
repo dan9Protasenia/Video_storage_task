2b8228bb976aeed83cffa062bbadc5b909e558cc
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-18 18:30:30 +0300

    refactor(router): edit some router for auth

--- src/app/api/user/router.py
+++ src/app/api/user/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
 from sqlalchemy.future import select
 
+from src.app.api.auth.dependencies import get_current_user
 from src.app.api.user.views import (
     delete_video_logic,
     get_video_logic,
@@ -9,23 +10,29 @@ from src.app.api.user.views import (
     upload_video_logic,
 )
 from src.app.infrastructure.database.database import AsyncSession, get_db
-from src.app.infrastructure.database.models.postgres import VideoModel
+from src.app.infrastructure.database.models.user_model import UserModel
+from src.app.infrastructure.database.models.video_model import VideoModel
 
 router = APIRouter()
 
 
 @router.post("/upload-video/")
 async def upload_video(
-        title: str = Form(...),
-        description: str = Form(...),
-        video: UploadFile = File(...),
-        db: AsyncSession = Depends(get_db),
+    title: str = Form(...),
+    description: str = Form(...),
+    video: UploadFile = File(...),
+    db: AsyncSession = Depends(get_db),
+    current_user: UserModel = Depends(get_current_user),
 ):
-    return await upload_video_logic(title, description, video, bucket_name="video", db=db)
+    return await upload_video_logic(video=video, title=title, description=description, bucket_name="video", db=db)
 
 
 @router.get("/{file_path}")
-async def get_video(file_path: str, db: AsyncSession = Depends(get_db)):
+async def get_video(
+    file_path: str,
+    db: AsyncSession = Depends(get_db),
+    current_user: UserModel = Depends(get_current_user),
+):
     result = await db.execute(select(VideoModel).where(VideoModel.file_path == file_path))
     video = result.scalars().first()
     if video is None:
@@ -34,12 +41,19 @@ async def get_video(file_path: str, db: AsyncSession = Depends(get_db)):
 
 
 @router.put("/{old_file_name}")
-async def update_video(old_file_name: str, video: UploadFile = File(...)):
+async def update_video(
+    old_file_name: str,
+    video: UploadFile = File(...),
+    current_user: UserModel = Depends(get_current_user),
+):
     return await update_video_logic(old_file_name, video, bucket_name="video")
 
 
 @router.delete("/{file_name}")
-async def delete_video(file_name: str):
+async def delete_video(
+    file_name: str,
+    current_user: UserModel = Depends(get_current_user),
+):
     return await delete_video_logic(file_name, bucket_name="video")
 
 
