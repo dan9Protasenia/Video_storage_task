55b6410a1ceee3e743ec5ee6ecbbbc15c65548ce
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-19 23:50:12 +0300

    refactor(security): add my personal Exception

--- src/app/core/modules/security.py
+++ src/app/core/modules/security.py
@@ -1,8 +1,9 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
+from src.app.core.handlers.errors import AuthError, ConflictError, UserNotFoundError
 from src.app.infrastructure.database.database import get_db
 from src.app.infrastructure.database.models.user_model import UserModel
 from src.app.security import auth
@@ -11,14 +12,14 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 async def get_current_user(db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    username = auth.verify_token(token, credentials_exception)
+
+    try:
+        username = auth.verify_token(token)
+    except AuthError as e:
+        raise ConflictError(detail=str(e))
+
     result = await db.execute(select(UserModel).filter(UserModel.username == username))
     user = result.scalars().first()
     if user is None:
-        raise credentials_exception
+        raise UserNotFoundError(detail="User not found.")
     return user
