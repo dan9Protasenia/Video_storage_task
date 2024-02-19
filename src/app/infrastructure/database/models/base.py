7d6fea502e70872b850710e81e6cd2480bcfe18e
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-20 02:58:19 +0300

    refactor(base): customization base

--- src/app/infrastructure/database/models/base.py
+++ src/app/infrastructure/database/models/base.py
@@ -1,3 +1,12 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 Base = declarative_base()
+
+
+class BaseTable(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
