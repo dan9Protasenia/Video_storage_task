8fb3af12cf3520fc5dd51ebe2618f8d02e40bc5b
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-20 23:54:49 +0300

    feat(base): add docstring

--- src/app/infrastructure/database/models/base.py
+++ src/app/infrastructure/database/models/base.py
@@ -6,6 +6,14 @@ Base = declarative_base()
 
 
 class BaseTable(Base):
+    """
+        Abstract base class for all models, providing default ID and timestamp fields.
+
+        Attributes:
+            id (Integer): The primary key for the record, automatically indexed.
+            created_at (DateTime): Timestamp of record creation, set to the current time by default.
+            updated_at (DateTime): Timestamp of the last record update, set to the current time upon updating.
+        """
     __abstract__ = True
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
