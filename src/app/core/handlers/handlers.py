ef707cdb0cd74c4f5e96a362c04ca704d4fbe68b
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-21 00:03:41 +0300

    refactor(handlers): create an instance in a method

--- src/app/core/handlers/handlers.py
+++ src/app/core/handlers/handlers.py
@@ -4,36 +4,41 @@ from fastapi.responses import JSONResponse
 from src.app.core.handlers.errors import AuthError, ConflictError, InternalServerError, RegError, UserNotFoundError
 
 
-async def conflict_error_handler(request: Request, exc: ConflictError):
+async def conflict_error_handler(request: Request, exc: Exception):
+    exc = ConflictError(str(exc))
     return JSONResponse(
         status_code=exc.status_code,
-        content={"message": exc.detail},
+        content={"message": exc.message},
     )
 
 
-async def auth_error_handler(request: Request, exc: AuthError):
+async def auth_error_handler(request: Request, exc: Exception):
+    exc = AuthError(str(exc))
     return JSONResponse(
         status_code=exc.status_code,
-        content={"message": exc.detail},
+        content={"message": exc.message},
     )
 
 
-async def user_not_found_error_handler(request: Request, exc: UserNotFoundError):
+async def user_not_found_error_handler(request: Request, exc: Exception):
+    exc = UserNotFoundError(str(exc))
     return JSONResponse(
         status_code=exc.status_code,
-        content={"message": exc.detail},
+        content={"message": exc.message},
     )
 
 
-async def reg_error_handler(request: Request, exc: RegError):
+async def reg_error_handler(request: Request, exc: Exception):
+    exc = RegError(str(exc))
     return JSONResponse(
         status_code=exc.status_code,
-        content={"message": exc.detail},
+        content={"message": exc.message},
     )
 
 
-async def server_error(request: Request, exc: InternalServerError):
+async def server_error(request: Request, exc: Exception):
+    exc = InternalServerError(str(exc))
     return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": exc.detail},
+
+        content={"message": exc.message},
     )
