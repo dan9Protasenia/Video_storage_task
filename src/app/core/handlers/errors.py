42878de9bf3f351316c26b747aa695af441bcc6c
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-20 23:58:05 +0300

    refactor(errors): add BaseError(Exception):

--- src/app/core/handlers/errors.py
+++ src/app/core/handlers/errors.py
@@ -1,26 +1,29 @@
-from fastapi import HTTPException, status
+class BaseError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
 
 
-class RegError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+class RegError(BaseError):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
-class AuthError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+class AuthError(BaseError):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
-class UserNotFoundError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+class UserNotFoundError(BaseError):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
-class ConflictError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+class ConflictError(BaseError):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
-class InternalServerError(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
+class InternalServerError(BaseError):
+    def __init__(self, message: str):
+        super().__init__(message)
