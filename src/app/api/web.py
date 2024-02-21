b51823d6f9b3b33c629c0ffc752e1409a059f3a6
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-21 19:01:13 +0300

    refactor(web): rm code in web

--- src/app/api/web.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from src.app.api.health import router as health_router
-from src.app.api.home import router as home_router
-
-router = APIRouter()
-
-router.include_router(home_router)
-router.include_router(health_router)
