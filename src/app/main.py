7d7298ca4d9ec230eb5f8753df36ffe821b5b567
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-21 19:00:30 +0300

    refactor(main): add if __name__ == "__main__":

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,8 @@
-from src.app.api.startup import init_routers, app
+import uvicorn
 
-init_routers(app)
+from src.app.startup import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
