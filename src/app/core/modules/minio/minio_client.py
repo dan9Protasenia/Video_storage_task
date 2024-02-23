94ce79a2a64502ec26648ad871512daf8ca55a53
Daniil Protasenia <danikprotaseney@gmail.com>
2024-02-23 20:49:25 +0300

    feat(minio_client): use env

--- src/app/core/modules/minio/minio_client.py
+++ src/app/core/modules/minio/minio_client.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 from botocore.client import Config
 
@@ -5,10 +7,10 @@ from botocore.client import Config
 def get_minio_client():
     s3_client = boto3.client(
         "s3",
-        endpoint_url="http://localhost:9000",
-        aws_access_key_id="minioadmin",
-        aws_secret_access_key="minioadmin",
+        endpoint_url=os.getenv("MINIO_ENDPOINT"),
+        aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
         config=Config(signature_version="s3v4"),
-        region_name="us-east-1",
+        region_name=os.getenv("MINIO_REGION_NAME"),
     )
     return s3_client
